f129641e8395610cc0a746816ece5b62d67e7a90
KurnosovNikita <Kurnosov-nikita@mail.ru>
2018-05-06 03:48:39 +0300

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,60 @@
+const mobileMenu = document.querySelector(".label-menu");
+const openMenu = document.querySelector(".field-nav ul");
+const navigationTab = document.querySelector(".field-nav ul");
+let clicked = 0;
+mobileMenu.addEventListener("click", function () {
+    if (clicked == 0) {
+        openMenu.className = "show-menu";
+        clicked = 1;
+    } else {
+        openMenu.className = "";
+        clicked = 0;
+    }
+});
+navigationTab.addEventListener("click", function (e) {
+    let a = e.target;
+    localStorage.setItem("123", e.target.innerHTML);
+    console.log(e.target);
+    switch (e.target.id) {
+        case "tab1":
+            for (let i = 0; i < 5; i++) {
+                document.querySelector("#tab" + (i + 1)).className = "";
+            }
+            document.querySelector("#tab1").className = "page-checked";
+            document.querySelector(".field-main-container").className = "d-flex field-main-container hide";
+            document.querySelector(".field-sidebar").className = "d-flex field-sidebar hide";
+            break;
+        case "tab2":
+            for (let i = 0; i < 5; i++) {
+                document.querySelector("#tab" + (i + 1)).className = "";
+            }
+            document.querySelector("#tab2").className = "page-checked";
+            document.querySelector(".field-main-container").className = "d-flex field-main-container hide";
+            document.querySelector(".field-sidebar").className = "d-flex field-sidebar hide";
+            break;
+        case "tab3":
+            for (let i = 0; i < 5; i++) {
+                document.querySelector("#tab" + (i + 1)).className = "";
+            }
+            document.querySelector("#tab3").className = "page-checked";
+            document.querySelector(".field-main-container").className = "d-flex field-main-container";
+            document.querySelector(".field-sidebar").className = "d-flex field-sidebar";
+            break;
+        case "tab4":
+            for (let i = 0; i < 5; i++) {
+                document.querySelector("#tab" + (i + 1)).className = "";
+            }
+            document.querySelector("#tab4").className = "page-checked";
+            document.querySelector(".field-main-container").className = "d-flex field-main-container hide";
+            document.querySelector(".field-sidebar").className = "d-flex field-sidebar hide";
+            break;
+        case "tab5":
+            for (let i = 0; i < 5; i++) {
+                document.querySelector("#tab" + (i + 1)).className = "";
+            }
+            document.querySelector("#tab5").className = "page-checked";
+            document.querySelector(".field-main-container").className = "d-flex field-main-container hide";
+            document.querySelector(".field-sidebar").className = "d-flex field-sidebar hide";
+            break;
+    }
+});
